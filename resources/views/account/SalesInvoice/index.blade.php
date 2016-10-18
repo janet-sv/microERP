91dd0eaed4ff604db480f0c64fc24c63ad706bd6
Alejandro <alejandro.rodriguez@pucp.pe>
2016-10-18 18:14:16 -0500

    add ventas

---
+++ resources/views/account/SalesInvoice/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.master')
+
+@section('title','Lista de Productos')
+
+@section('content')
+
+   <!-- Main component for a primary marketing message or call to action -->
+   
+  <ol class="breadcrumb">
+     <li><a href="{{url('dashboard')}}">Escritorio</a></li>
+     <li class="active">Productos</li>
+   </ol>
+ 
+
+   <div class="page-header">
+     <h1>Productos <small>Actualizados hasta hoy</small></h1>
+   </div>
+
+   <div class="row">
+     <div class="col-md-8">
+
+       @include('partials.messages') 
+        <div class="panel panel-default">
+          <div class="panel-heading">
+             Lista
+             <p class="navbar-text navbar-right" style=" margin-top: 1px;">
+                <button type="button" id='nuevo'  name='nuevo' class="btn btn-warning navbar-btn" style="margin-bottom: 1px; margin-top: -5px;margin-right: 8px;padding: 3px 20px;">Nuevo</button>
+             </p>
+           </div>
+          <div class="panel-body">
+               <div id="list-product"></div>
+
+
+          </div>
+        </div>
+
+
+     </div>
+   </div>
+
+<script  type="text/javascript">
+
+
+    $(document).ready(function(){
+        listProduct();
+    });
+
+
+    $(document).on("click",".pagination li a",function(e) {
+        e.preventDefault();
+
+        var url = $(this).attr("href");
+
+        $.ajax({
+            type:'get',
+            url:url,
+            success: function(data){
+                $('#list-product').empty().html(data);
+            }
+        });
+
+    });
+
+
+
+
+    $("#nuevo").click(function(event)
+  {
+      document.location.href = "{{ route('product.create')}}";
+  });
+
+  var listProduct = function()
+  {
+      $.ajax({
+          type:'get',
+          url:'{{ url('listall')}}',
+          success: function(data){
+              $('#list-product').empty().html(data);
+          }
+      });
+  }
+
+
+</script>
+  
+@endsection
